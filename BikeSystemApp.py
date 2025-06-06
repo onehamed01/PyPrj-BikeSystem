49856a9c1566573af3d6c98045eed6256f3208c1
onehamed01 <onehamed01@Gmail.com>
2025-06-06 19:11:51 +0100

    update bikes in stock

--- BikeSystemApp.py
+++ BikeSystemApp.py
@@ -8,7 +8,10 @@ class BikeSystem:
         }
         self.bikes_stock = 10
 
-                
+    @property
+    def viewStock(self):
+        print("total Bikes:",self.bikes_stock) 
+
     def rentBike(self, type_rent, count_type_rent):
         while True:
             if self.bike_count > self.bikes_stock:
@@ -22,14 +25,13 @@ class BikeSystem:
 
         self.bikes_stock -= self.bike_count
         print(f"you have rented {self.bike_count} Bikes, for {count_type_rent} {type_rent}\ntotal price is {total_price} £")
-        
+        print(f"total Bikes: {self.bikes_stock}")
+
     def returnBike(self, count_return_bikes):
         if count_return_bikes != self.bike_count:
             print("you didn't return all bikes")
         else:
             self.bikes_stock += count_return_bikes
+            print("Thank you for your return")
         
-    @property
-    def viewStock(self):
-        print("total Bikes:",self.bikes_stock)
 
