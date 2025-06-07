c5e8e9a2bab07b97828e54d3f6a18329abfd6ebe
onehamed01 <onehamed01@Gmail.com>
2025-06-07 11:03:21 +0100

    update discount. remove while stock check.add error handling type rent

--- BikeSystemApp.py
+++ BikeSystemApp.py
@@ -1,4 +1,5 @@
 class BikeSystem:
+    bikes_stock = 10
     def __init__(self, count_bikes):
         self.bike_count = count_bikes
         self.type_rent = {
@@ -6,22 +7,17 @@ class BikeSystem:
             'day':20,
             'week':60
         }
-        self.bikes_stock = 10
-
-    @property
-    def viewStock(self):
-        print("total Bikes:",self.bikes_stock) 
 
     def rentBike(self, type_rent, count_type_rent):
-        while True:
-            if self.bike_count > self.bikes_stock:
-                print(f"you need to rent less than {self.bikes_stock}")
-            else:
-                break
-
-        total_price = self.type_rent[type_rent] * count_type_rent * self.bike_count
-        if 3 <= self.bike_count <= 5: 
-            total_price = 0.30 * total_price
+        if self.bike_count > self.bikes_stock:
+            print(f"you need to rent less than {self.bikes_stock}")
+        if type_rent not in self.type_rent:
+            print("Invalid Type rent")
+            return
+        else:
+            total_price = self.type_rent[type_rent] * count_type_rent * self.bike_count
+            if 3 <= self.bike_count <= 5: 
+                total_price *= 0.7
 
         self.bikes_stock -= self.bike_count
         print(f"you have rented {self.bike_count} Bikes, for {count_type_rent} {type_rent}\ntotal price is {total_price} £")
@@ -34,4 +30,3 @@ class BikeSystem:
             self.bikes_stock += count_return_bikes
             print("Thank you for your return")
         
-
